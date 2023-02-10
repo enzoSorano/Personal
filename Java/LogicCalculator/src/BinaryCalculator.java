81493a2446234d45d0fabf948df2f74733a49c9f
enzsoran <enzo.sorano@du.edu>
2023-02-09 22:37:10 -0700

    multiplication Done
    multiplication done

--- Java/LogicCalculator/src/BinaryCalculator.java
+++ Java/LogicCalculator/src/BinaryCalculator.java
@@ -5,6 +5,40 @@
  * This is the skeleton code form COMP2691 Assignment #2.
  */
 public class BinaryCalculator {
+	 //**helper function to find twos complement
+	public static BitField twosComplement(BitField b) {
+		for (int i = 0; i < b.size(); i++) {
+			if (b.get(i) == true) {
+				b.set(i, false);
+			} else {
+				b.set(i, true);
+			}
+		}
+		BitField temp = new BitField(b.size(), "1");
+		BitField twosComplementB = add(temp, b);
+		
+		return twosComplementB;
+	}
+	//**helper function to left shift a bitfield
+	public static BitField leftShift(BitField a, int shiftAmount) {
+		//** take in the bitfield and make a copy to manipulate
+		BitField value = new BitField(a.size());
+		value = a.copy();
+		
+		//** shift the bits
+		for(int i =shiftAmount; i < value.size() ; i++ ) {
+			value.set(i, a.get(i - shiftAmount));
+		}
+		
+		
+		//**make the first bit equal to zero
+				for(int i =0; i < shiftAmount ; i++) {
+					value.set(i, false);
+				}
+				
+		return value;
+	}
+	
 	public static BitField add(BitField a, BitField b) {
 		if (null == a || null == b || a.size() != b.size()) {
 			throw new IllegalArgumentException(
@@ -14,7 +48,7 @@ public class BinaryCalculator {
 		BitField c = new BitField(a.size());
 		// **create a bit field for the result
 		BitField result = new BitField(a.size());
-
+		
 		// **iterate through the bitfield
 		for (int i = 0; i < a.size(); i++) {
 
@@ -43,6 +77,7 @@ public class BinaryCalculator {
 			throw new IllegalArgumentException(
 					"BinaryCalculator.add(a,b): a and b cannot be null and must be the same length.");
 		}
+		
 		// ** find two complement of the subtracting number
 		for (int i = 0; i < b.size(); i++) {
 			if (b.get(i) == true) {
@@ -63,11 +98,38 @@ public class BinaryCalculator {
 			throw new IllegalArgumentException(
 					"BinaryCalculator.add(a,b): a and b cannot be null and must be the same length.");
 		}
-		//**bitField to return
+		
+		//**BitFields that are double the size of the input fields
+		BitField DoubleA = new BitField(a.size()*2, a.toString());
+		BitField DoubleB = new BitField(b.size()*2, b.toString());
+
+		//**used to manipulate bit string b
+		BitField newB = new BitField(a.size()*2);
 		BitField result = new BitField(a.size()*2);
+
+	
 		for(int i =0; i < a.size(); i++) {
+			
+			if(a.get(i) == true) {
+				//**create a copy of the bitField and and left shift it 1
+				newB = DoubleB.copy();
+				newB = leftShift(newB, i);
+			} else {
+				//**create a BitField of all zeros
+				newB = new BitField(DoubleB.size());
+			}
+			result = add(newB,result);
+			
+			
 		}
-		return result;
+		//** make the bitfield the appropriate size
+		BitField value = new BitField(a.size());
+		for(int i =0; i < value.size(); i++) {
+			//System.out.println(result.get(i));
+			value.set(i, result.get(i));
+		}
+		
+		return value;
 	}
 
 	public static BitField[] divide(BitField a, BitField b) {
--- Java/LogicCalculator/src/testDriver.java
+++ Java/LogicCalculator/src/testDriver.java
@@ -4,11 +4,27 @@ public class testDriver {
 	public static void main(String args[]) {
 		//**test bitfields and BinaryCalculator
 		BinaryCalculator bc = new BinaryCalculator();
-		BitField a = new BitField("1011");
-		BitField b = new BitField("1110");
+		BitField a = new BitField("0111010000111100110101111110110000110111101101");
+		BitField b = new BitField("0100110011101011010110101000100011111100011011");
+		/*
+		BitField DoubleA = new BitField(a.size()*2, a.toString());
+		System.out.println(bc.leftShift(DoubleA, 3));
+		*/
 		
-		bc.multiply(a, b);
+		/*
+		System.out.println(a.toString());
+		System.out.println(bc.leftShift(a, 2).toString());
+		System.out.println(bc.add(a,b));
+		*/
+		System.out.println(bc.multiply(a, b).toString());
+		String expected = "0101000001100001100111111000011100101011111111";
+		String actual = "00100010111011001110110100001111111011010011100101000001100001100111111000011100101011111111";
+		//System.out.println(expected.length() + "\n" + actual.length());
+		//System.out.println("\n" + "0101000001100001100111111000011100101011111111");
 
+
+		
+		
 
 		
 	}
